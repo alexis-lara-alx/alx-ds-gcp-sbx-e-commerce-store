ef8d325e6c8bc1c15fc031f483abe4b52ca29055
AlexDeLara <hfdelara@gmail.com>
2025-01-30 19:52:16 -0600

    Add stn tables constraints

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "google_bigquery_dataset" "stn_ecommerce" {
 	dataset_id = "stn_ecommerce"
-	description = "stn E-Commerce data"
+	description = "Standardized E-Commerce data"
 	location = "US"
 	is_case_insensitive = true
 
@@ -12,7 +12,7 @@ resource "google_bigquery_dataset" "stn_ecommerce" {
 	}
 }
 
-resource "google_bigquery_table" "stn_ecommerce_customer" {
+resource "google_bigquery_table" "stn_ecommerce_customers" {
 	dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
 	table_id = "olist_customers_dataset"
 	description = "This dataset has information about the customer and its location. Use it to identify unique customers in the orders dataset and to find the orders delivery location"
@@ -22,13 +22,13 @@ resource "google_bigquery_table" "stn_ecommerce_customer" {
 			{
 				"name": "customer_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
-				"description": "Key to the orders dataset. Each order has a unique customer_id"
+				"mode": "REQUIRED",
+				"description": "Key to the orders dataset. Each order has a unique order_customer_id"
 			},
 			{
 				"name": "customer_unique_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Unique identifier of a customer"
 			},
 			{
@@ -52,6 +52,23 @@ resource "google_bigquery_table" "stn_ecommerce_customer" {
 		]
 		EOF
 
+	primary_key {
+		columns = ["customer_id"]
+	}
+
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_geolocation.table_id
+		}
+
+		column_references {
+			refencing_column = "customer_zip_code_prefix"
+			referenced_column = "geolocation_zip_code_prefix"
+		}
+	}
+
 	labels = {
 		project = "ecommerce-store"
 		env = "sandbox"
@@ -70,19 +87,19 @@ resource "google_bigquery_table" "stn_ecommerce_geolocation" {
 			{
 				"name": "geolocation_zip_code_prefix",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "First 5 digits of zip code"
 			},
 			{
 				"name": "geolocation_lat",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "NUMERIC(17,15)",
+				"mode": "REQUIRED",
 				"description": "Latitude"
 			},
 			{
 				"name": "geolocation_lng",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "NUMERIC(17,15)",
+				"mode": "REQUIRED",
 				"description": "Longitude"
 			},
 			{
@@ -100,6 +117,10 @@ resource "google_bigquery_table" "stn_ecommerce_geolocation" {
 		]
 		EOF
 
+	primary_key {
+		columns = ["geolocation_zip_code_prefix"]
+	}
+	
 	labels = {
 		project = "ecommerce-store"
 		env = "sandbox"
@@ -118,47 +139,90 @@ resource "google_bigquery_table" "stn_ecommerce_order_items" {
 			{
 				"name": "order_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED"
 				"description": "Order unique identifier"
 			},
 			{
 				"name": "order_item_id",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "INTEGER",
+				"mode": "REQUIRED",
 				"description": "Sequential number identifying number of items included in the same order"
 			},
 			{
 				"name": "product_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Product unique identifier"
 			},
 			{
 				"name": "seller_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Seller unique identifier"
 			},
 			{
 				"name": "shipping_limit_date",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "TIMESTAMP",
+				"mode": "REQUIRED",
 				"description": "Shows the seller shipping limit date for handling the order over to the logistic partner"
 			},
 			{
 				"name": "price",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "NUMERIC",
+				"mode": "REQUIRED",
 				"description": "Item price"
 			},
 			{
 				"name": "freight_value",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "NUMERIC",
+				"mode": "REQUIRED",
 				"description": "Item freight value item (if an order has more than one item the freight value is splitted between items)"
 			}
 		]
 		EOF
+
+	primary_key {
+		columns = ["order_id", "order_item_id"]
+	}
+
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_orders.table_id
+		}
+
+		column_references {
+			refencing_column = "order_id"
+			referenced_column = "order_id"
+		}
+	}
+
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_products.table_id
+		}
+
+		column_references {
+			refencing_column = "product_id"
+			referenced_column = "product_id"
+		}
+	}
+	
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_sellers.table_id
+		}
+
+		column_references {
+			refencing_column = "seller_id"
+			referenced_column = "seller_id"
+		}
+	}
 
 	labels = {
 		project = "ecommerce-store"
@@ -178,36 +242,53 @@ resource "google_bigquery_table" "stn_ecommerce_order_payments" {
 			{
 				"name": "order_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Unique identifier of an order"
 			},
 			{
 				"name": "payment_sequential",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "INTEGER",
+				"mode": "REQUIRED",
 				"description": "A customer may pay an order with more than one payment method. If he does so, a sequence will be created to"
 			},
 			{
 				"name": "payment_type",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Method of payment chosen by the customer"
 			},
 			{
 				"name": "payment_installments",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "INTEGER",
+				"mode": "REQUIRED",
 				"description": "Number of installments chosen by the customer"
 			},
 			{
 				"name": "payment_value",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "NUMERIC",
+				"mode": "REQUIRED",
 				"description": "Transaction value"
 			}
 		]
 		EOF
 
+	primary_key {
+		columns = ["order_id", "payment_sequential"]
+	}
+
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_orders.table_id
+		}
+
+		column_references {
+			refencing_column = "order_id"
+			referenced_column = "order_id"
+		}
+	}
+	
 	labels = {
 		project = "ecommerce-store"
 		env = "sandbox"
@@ -226,19 +307,19 @@ resource "google_bigquery_table" "stn_ecommerce_order_reviews" {
 			{
 				"name": "review_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Unique review identifier"
 			},
 			{
 				"name": "order_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Unique order identifier"
 			},
 			{
 				"name": "review_score",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "INTEGER",
+				"mode": "REQUIRED",
 				"description": "Note ranging from 1 to 5 given by the customer on a satisfaction survey"
 			},
 			{
@@ -256,19 +337,36 @@ resource "google_bigquery_table" "stn_ecommerce_order_reviews" {
 			,
 			{
 				"name": "review_creation_date",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "TIMESTAMP",
+				"mode": "REQUIRED",
 				"description": "Shows the date in which the satisfaction survey was sent to the customer"
 			},
 			{
 				"name": "review_answer_timestamp",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "TIMESTAMP",
+				"mode": "REQUIRED",
 				"description": "Shows satisfaction survey answer timestamp"
 			}
 		]
 		EOF
 
+	primary_key {
+		columns = ["review_id"]
+	}
+
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_orders.table_id
+		}
+
+		column_references {
+			refencing_column = "order_id"
+			referenced_column = "order_id"
+		}
+	}
+	
 	labels = {
 		project = "ecommerce-store"
 		env = "sandbox"
@@ -287,55 +385,72 @@ resource "google_bigquery_table" "stn_ecommerce_orders" {
 			{
 				"name": "order_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Unique identifier of the order"
 			},
 			{
 				"name": "customer_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Key to the customer dataset. Each order has a unique customer_id"
 			},
 			{
 				"name": "order_status",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Reference to the order status (delivered, shipped, etc)"
 			},
 			{
 				"name": "order_purchase_timestamp",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "TIMESTAMP",
+				"mode": "REQUIRED",
 				"description": "Shows the purchase timestamp"
 			},
 			{
 				"name": "order_approved_at",
-				"type": "STRING",
+				"type": "TIMESTAMP",
 				"mode": "NULLABLE",
 				"description": "Shows the payment approval timestamp"
 			}
 			,
 			{
 				"name": "order_delivered_carrier_date",
-				"type": "STRING",
+				"type": "TIMESTAMP",
 				"mode": "NULLABLE",
 				"description": "Shows the order posting timestamp. When it was handled to the logistic partner"
 			},
 			{
 				"name": "order_delivered_customer_date",
-				"type": "STRING",
+				"type": "TIMESTAMP",
 				"mode": "NULLABLE",
 				"description": "Shows the actual order delivery date to the customer"
 			},
 			{
 				"name": "order_estimated_delivery_date",
-				"type": "STRING",
-				"mode": "NULLABLE",
+				"type": "TIMESTAMP",
+				"mode": "REQUIRED",
 				"description": "Shows the estimated delivery date that was informed to customer at the purchase moment"
 			}
 		]
 		EOF
 
+	primary_key {
+		columns = ["order_id"]
+	}
+
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_customers.table_id
+		}
+
+		column_references {
+			refencing_column = "customer_id"
+			referenced_column = "customer_id"
+		}
+	}
+	
 	labels = {
 		project = "ecommerce-store"
 		env = "sandbox"
@@ -354,7 +469,7 @@ resource "google_bigquery_table" "stn_ecommerce_products" {
 			{
 				"name": "product_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Unique product identifier"
 			},
 			{
@@ -365,50 +480,54 @@ resource "google_bigquery_table" "stn_ecommerce_products" {
 			},
 			{
 				"name": "product_name_lenght",
-				"type": "STRING",
+				"type": "INTEGER",
 				"mode": "NULLABLE",
 				"description": "Number of characters extracted from the product name"
 			},
 			{
 				"name": "product_description_lenght",
-				"type": "STRING",
+				"type": "INTEGER",
 				"mode": "NULLABLE",
 				"description": "Number of characters extracted from the product description"
 			},
 			{
 				"name": "product_photos_qty",
-				"type": "STRING",
+				"type": "INTEGER",
 				"mode": "NULLABLE",
 				"description": "Number of product published photos"
 			}
 			,
 			{
 				"name": "product_weight_g",
-				"type": "STRING",
+				"type": "INTEGER",
 				"mode": "NULLABLE",
 				"description": "Product weight measured in grams"
 			},
 			{
 				"name": "product_length_cm",
-				"type": "STRING",
+				"type": "INTEGER",
 				"mode": "NULLABLE",
 				"description": "Product length measured in centimeters"
 			},
 			{
 				"name": "product_height_cm",
-				"type": "STRING",
+				"type": "INTEGER",
 				"mode": "NULLABLE",
 				"description": "Product height measured in centimeters"
 			},
 			{
 				"name": "product_width_cm",
-				"type": "STRING",
+				"type": "INTEGER",
 				"mode": "NULLABLE",
 				"description": "product width measured in centimeters"
 			}
 		]
 		EOF
 
+	primary_key {
+		columns = ["product_id"]
+	}
+	
 	labels = {
 		project = "ecommerce-store"
 		env = "sandbox"
@@ -427,59 +546,46 @@ resource "google_bigquery_table" "stn_ecommerce_sellers" {
 			{
 				"name": "seller_id",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Seller unique identifier"
 			},
 			{
 				"name": "seller_zip_code_prefix",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "First 5 digits of seller zip code"
 			},
 			{
 				"name": "seller_city",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Seller city name"
 			},
 			{
 				"name": "seller_state",
 				"type": "STRING",
-				"mode": "NULLABLE",
+				"mode": "REQUIRED",
 				"description": "Seller state"
 			}
 		]
 		EOF
 
-	labels = {
-		project = "ecommerce-store"
-		env = "sandbox"
-		customer = "alx-ds"
-		lake_zone = "stn"
+	primary_key {
+		columns = ["seller_id"]
 	}
-}
 
-resource "google_bigquery_table" "stn_ecommerce_sellers" {
-	dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
-	table_id = "product_category_name_translation"
-	description = "Translates the product_category_name to English"
+	foreign_keys {
+		referenced_table {
+			project_id = google_bigquery_dataset.stn_ecommerce.project
+			dataset_id = google_bigquery_dataset.stn_ecommerce.dataset_id
+			table_id = google_bigquery_table.stn_ecommerce_geolocation.table_id
+		}
 
-	schema = <<EOF
-		[
-			{
-				"name": "product_category_name",
-				"type": "STRING",
-				"mode": "NULLABLE",
-				"description": "Category name in Portuguese"
-			},
-			{
-				"name": "product_category_name_english",
-				"type": "STRING",
-				"mode": "NULLABLE",
-				"description": "Category name in English"
-			}
-		]
-		EOF
+		column_references {
+			refencing_column = "seller_zip_code_prefix"
+			referenced_column = "geolocation_zip_code_prefix"
+		}
+	}
 
 	labels = {
 		project = "ecommerce-store"
